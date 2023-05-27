1bfec01091fd7d773a1dba7f26bf1ebea8daa857
SafaHassoun <safa.21.hassoun@gmail.com>
2023-05-27 15:29:50 +0300

    add icons

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import {View,Text} from "react-native"
-
+import { View, Text } from "react-native"
+import Icon from 'react-native-vector-icons/FontAwesome';
 export default function App() {
+ 
   return (
     <View>
-      <Text>Hello world</Text>
+      <Text>Hello
+        <Icon name="home" size={30} color="#900" />
+      </Text>
     </View>
   )
 }
