5fd77d3e654327421b141235f1c34763932749ac
SafaHassoun <safa.21.hassoun@gmail.com>
2023-05-31 21:17:36 +0300

    Modification in App.js , CureentWeather.js, added a new component OtherCities.js

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,36 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 import { View, Button, Text, ImageBackground, StyleSheet, TouchableOpacity } from 'react-native';
 import MI from 'react-native-vector-icons/MaterialCommunityIcons';
 import axios from 'axios';
 
-export default function CurrentWeather({ navigation }) {
-    const [data, setData] = useState([]);
 
-    // useEffect(() => {
-    //     axios.get('http://api.weatherapi.com/v1/current.json?key=1469bcf832b14b239c9114030232705&q=Tripoli-Lebanon&aqi=no')
-    //         .then((res) => {
-    //             this.setData({
-    //                 data: res.data
-    //             })
-    //         })
-    // }, [data])
+export default function CurrentWeather({ navigation }) {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchWeather = async()=>{
+            try{
+                const res= await axios.get('http://api.weatherapi.com/v1/current.json?key=1469bcf832b14b239c9114030232705&q=Tripoli-Lebanon&aqi=no');
+                setData(res.data);
+            }catch(error){
+                console.log('Error fetching weather data:', error);
+            }
+        };
+        fetchWeather();
+    }, []);
+
+    if (!data) {
+        return <Text style={{ flex: 1, textAlign:"center" }}>Loading...</Text>;
+      }
+
 
     return (
-        <ImageBackground source={{ uri: 'https://reactjs.org/logo-og.png' }} style={styles.image}>
+        <ImageBackground source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7vFpfZSjG1TiOCrvGcgo0JvbxRWvLeKCZmw&usqp=CAU' }} style={styles.image}>
             <View style={styles.container}>
                 <View style={{ flex: 2 }}>
-                    <Text style={styles.text} >Monday, 25 May 2023</Text>
-                    <Text style={[styles.text, { fontSize: 35, fontStyle: "normal" }]} >Tripoli</Text>
+                    <Text style={styles.text} >Tuesday, 30 May 2023</Text>
+                    <Text style={[styles.text, { fontSize: 30, fontStyle: "normal" }]} >{data.location.name}</Text>
                 </View>
 
                 <View style={{ flex: 2, flexDirection: "row" }}>
-                    <Text style={{ fontSize: 90, color: "white" }} >22*</Text>
-                    <MI name="weather-lightning-rainy" size={100} color="white" />
+                    <Text style={{ fontSize: 60, color: "white" ,margin:10}} >{data.current.temp_c}°</Text>
+                    <MI name="weather-lightning-rainy" size={100} color="white" style={{margin:20}}/>
                 </View>
 
                 <View style={{ flex: 4 }}>
-                    <Text style={styles.text} >Feels like 22*</Text>
-                    <Text style={styles.text} >Cloudy</Text>
+                    <Text style={styles.text} >Feels like {data.current.feelslike_c}°</Text>
+                    <Text style={styles.text} >{data.current.condition.text}</Text>
                 </View>
 
                 <View style={{ flex: 2, flexDirection: "row", alignItems: 'center', justifyContent: 'center' }}>
@@ -59,7 +68,7 @@ const styles = StyleSheet.create({
     },
     text: {
         color: "white",
-        fontSize: 15,
+        fontSize: 12,
         padding: 5,
         fontStyle: "italic"
     },
@@ -72,6 +81,8 @@ const styles = StyleSheet.create({
         borderRadius: 50
     }
 })
+
+
 
 
 // <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
