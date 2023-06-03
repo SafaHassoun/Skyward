75dbb02d8d01842ec3ca6722873ddcf3e2b72d16
Abdulkaderadams <abdulkaderadams1@gmail.com>
2023-06-03 09:43:30 +0300

    weather details
    fetch hourly data

--- App.js
+++ App.js
@@ -1,40 +1,48 @@
-import React from "react";
-import { View, Text } from "react-native"
+import React from 'react';
+import {View, Text} from 'react-native';
 //import FA from 'react-native-vector-icons/FontAwesome';
 //import MI from 'react-native-vector-icons/MaterialCommunityIcons';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import CurrentWeather from "./src/components/CurrentWeather";
-import WeatherDetails from "./src/components/WeatherDetails";
-import Forecasts from "./src/components/Forecasts";
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import CurrentWeather from './src/components/CurrentWeather';
+import WeatherDetails from './src/components/WeatherDetails';
+import Forecasts from './src/components/Forecasts';
 
 const Stack = createStackNavigator();
 
 function MyStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={CurrentWeather} options={{title:'Home'}}/>
-      <Stack.Screen name="WeatherDetails" component={WeatherDetails} options={{title:'Weather Details'}} />
-      <Stack.Screen name="Forecasts" component={Forecasts} options={{title:'Forecast'}} />
+      <Stack.Screen
+        name="Home"
+        component={CurrentWeather}
+        options={{title: 'Home'}}
+      />
+      <Stack.Screen
+        name="WeatherDetails"
+        component={WeatherDetails}
+        options={{title: 'Weather Details'}}
+      />
+      <Stack.Screen
+        name="Forecasts"
+        component={Forecasts}
+        options={{title: 'Forecast'}}
+      />
     </Stack.Navigator>
   );
 }
 
 export default function App() {
- 
   return (
     <NavigationContainer>
       <MyStack />
     </NavigationContainer>
-    
-  )
+  );
 }
 
-
-
 // <View>
-    //   <Text>Hello
-    //     <FA name="home" size={30} color="#900" />
-    //     <MI name="weather-lightning-rainy" size={30} color="#900" />
-    //   </Text>
-    // </View>
+//   <Text>Hello
+//     <FA name="home" size={30} color="#900" />
+//     <MI name="weather-lightning-rainy" size={30} color="#900" />
+//   </Text>
+// </View>
--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -1,48 +1,184 @@
 import React, {useEffect, useState} from 'react';
-import {View, Button, Text} from 'react-native';
+import {
+  View,
+  Text,
+  PermissionsAndroid,
+  ImageBackground,
+  ActivityIndicator,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 import axios from 'axios';
+import {request, PERMISSIONS, RESULTS} from 'react-native-permissions';
 
-export default function WeatherDetails({navigation}) {
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 5,
+    elevation: 2,
+  },
+  container: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+  },
+  text: {
+    color: 'white',
+    fontSize: 15,
+    padding: 5,
+    fontStyle: 'italic',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  button: {
+    margin: 20,
+    borderRadius: 50,
+    backgroundColor: 'blue',
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  hourlyCard: {
+    backgroundColor: 'white',
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 5,
+    elevation: 2,
+  },
+  hourlyText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+});
+
+const WeatherApp = () => {
   const [weatherData, setWeatherData] = useState(null);
+  const [showAllHours, setShowAllHours] = useState(false);
+  const apiKey = '1469bcf832b14b239c9114030232705';
+  const maxDisplayedHours = 5;
 
   useEffect(() => {
-    const fetchWeatherData = async () => {
-      try {
-        const response = await axios.get(
-          'https://api.weatherapi.com/v1/current.json?key=1469bcf832b14b239c9114030232705&q=lebanon',
-        );
-        setWeatherData(response.data);
-      } catch (error) {
-        console.log('Error fetching weather data:', error);
-      }
-    };
-
-    fetchWeatherData();
+    requestLocationPermission();
   }, []);
 
-  if (!weatherData) {
-    return <Text>Loading...</Text>;
-  }
+  const requestLocationPermission = async () => {
+    try {
+      let permissionStatus;
+      if (Platform.OS === 'android') {
+        permissionStatus = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        );
+      } else if (Platform.OS === 'ios') {
+        permissionStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+      }
 
-  const {location, current} = weatherData;
+      if (permissionStatus === RESULTS.GRANTED) {
+        getCurrentLocation();
+      } else {
+        console.log('Location permission denied');
+      }
+    } catch (error) {
+      console.error('Error requesting location permission:', error);
+    }
+  };
+
+  const getCurrentLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        fetchWeatherData(latitude, longitude);
+      },
+      error => {
+        console.error('Error getting current location:', error);
+      },
+    );
+  };
+
+  const fetchWeatherData = async (latitude, longitude) => {
+    try {
+      const response = await axios.get(
+        `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${latitude},${longitude}&days=1&aqi=no&alerts=no`,
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
+  const toggleShowAllHours = () => {
+    setShowAllHours(!showAllHours);
+  };
+
+  if (!weatherData) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+  const location = weatherData.location.name;
+  const condition = weatherData.current.condition.text;
+  const temperature = weatherData.current.temp_c;
+  const humidity = weatherData.current.humidity;
+
+  const hourlyForecast = weatherData.forecast.forecastday[0].hour;
+  const displayedHours = showAllHours
+    ? hourlyForecast
+    : hourlyForecast.slice(0, maxDisplayedHours);
 
   return (
-    //<View>
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>
-        Location: {location.name}, {location.region}, {location.country}
-      </Text>
-      <Text>Temperature: {current.temp_c}°C</Text>
-      <Text>Condition: {current.condition.text}</Text>
-      <Text>Humidity: {current.humidity}%</Text>
-      <Text>Wind: {current.wind_kph} km/h</Text>
-
-      <Button
-        title="Go to Forecasts"
-        onPress={() => navigation.navigate('Forecasts')}
-      />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+    <View style={{flex: 1}}>
+      <ImageBackground
+        source={{uri: 'https://reactjs.org/logo-og.png'}}
+        style={styles.image}>
+        <ScrollView contentContainerStyle={styles.container}>
+          <View style={styles.centerItems}>
+            <View style={styles.card}>
+              <Text>Weather in: {location}</Text>
+            </View>
+            <View style={styles.card}>
+              <Text>Condition: {condition}</Text>
+            </View>
+            <View style={styles.card}>
+              <Text>Temperature: {temperature}°C</Text>
+            </View>
+            <View style={styles.card}>
+              <Text>Humidity: {humidity}%</Text>
+            </View>
+            {displayedHours.map(hour => (
+              <View key={hour.time} style={styles.hourlyCard}>
+                <Text style={styles.hourlyText}>Time: {hour.time}</Text>
+                <Text>Condition: {hour.condition.text}</Text>
+                <Text>Temperature: {hour.temp_c}°C</Text>
+                <Text>Humidity: {hour.humidity}%</Text>
+              </View>
+            ))}
+            {!showAllHours && hourlyForecast.length > maxDisplayedHours && (
+              <TouchableOpacity
+                style={styles.button}
+                onPress={toggleShowAllHours}>
+                <Text style={styles.buttonText}>Show More Hours</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+        </ScrollView>
+      </ImageBackground>
     </View>
-    //</View>
   );
-}
+};
+
+export default WeatherApp;
