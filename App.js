0f5055cd9d18b2c447a0000471eb07ae630c0181
SafaHassoun <safa.21.hassoun@gmail.com>
2023-06-10 11:57:05 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,40 +1,54 @@
-import React from 'react';
-import { View, Text, Button, TouchableOpacity, SafeAreaView, FlatList } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import React, {useState} from 'react';
+import {TouchableOpacity} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 import CurrentWeather from './src/components/CurrentWeather';
 import WeatherDetails from './src/components/WeatherDetails';
 import Forecasts from './src/components/Forecasts';
 import NI from 'react-native-vector-icons/FontAwesome';
-import SideMenu from './src/components/SideMenu';
 
 const Stack = createStackNavigator();
 
-function MyStack() {
+function AppStack() {
+  const [showSideMenu, setShowSideMenu] = useState(false);
+  const [selectedCity, setSelectedCity] = useState(null);
   return (
     <Stack.Navigator>
       <Stack.Screen
         name="Home"
-        component={CurrentWeather}
         options={{
           title: 'Home',
           headerRight: () => (
-            <SideMenu/>
-            // <TouchableOpacity onPress={() => alert("hello")} >
-            //   <NI name="navicon" size={40} color="black" backgroundColor="white" style={{marginRight:20}}/>
-            // </TouchableOpacity>
+            <TouchableOpacity onPress={() => setShowSideMenu(!showSideMenu)}>
+              <NI
+                name="navicon"
+                size={40}
+                color={showSideMenu ? 'red' : 'black'}
+                backgroundColor="white"
+                style={{marginRight: 20}}
+              />
+            </TouchableOpacity>
           ),
-        }}
-      />
+        }}>
+        {_props => (
+          <CurrentWeather
+            {..._props}
+            selectedCity={selectedCity}
+            setSelectedCity={setSelectedCity}
+            showSideMenu={showSideMenu}
+            setShowSideMenu={setShowSideMenu}
+          />
+        )}
+      </Stack.Screen>
       <Stack.Screen
         name="WeatherDetails"
         component={WeatherDetails}
-        options={{ title: 'Weather Details' }}
+        options={{title: 'Weather Details'}}
       />
       <Stack.Screen
         name="Forecasts"
         component={Forecasts}
-        options={{ title: 'Forecast' }}
+        options={{title: 'Forecast'}}
       />
     </Stack.Navigator>
   );
@@ -43,7 +57,7 @@ function MyStack() {
 export default function App() {
   return (
     <NavigationContainer>
-      <MyStack />
+      <AppStack />
     </NavigationContainer>
   );
 }
